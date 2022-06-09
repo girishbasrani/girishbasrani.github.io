e9ef087671026bc43ee47e6da85c4e4872bae105
Girish <girish@Girishs-Air.routerbbfd9d.com>
2022-06-09 15:17:51 +0530

    color change text on click

---
+++ color.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function()
+    {
+      //CHnage  font color to red  
+      document.querySelector('#red').onclick = function()
+            {
+                document.querySelector('#test').style.color='red';           
+            }
+    
+      {      
+      document.querySelector('#blue').onclick = function()
+            {
+                document.querySelector('#test').style.color='blue';           
+            }
+   
+          }  
+
+      
+    
+    }
+)

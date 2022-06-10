e9bcbbec6066eaa76810f1143f1c23566e6eaec5
Girish <girish@Girishs-Air.routerbbfd9d.com>
2022-06-10 13:10:05 +0530

    green

--- color.js
+++ color.js
@@ -6,13 +6,35 @@ document.addEventListener('DOMContentLoaded', function()
                 document.querySelector('#test').style.color='red';           
             }
     
-      {      
+      {     
+        //CHnage  font color to blue  
       document.querySelector('#blue').onclick = function()
             {
                 document.querySelector('#test').style.color='blue';           
             }
    
-          }  
+              
+
+      //CHnage  font color to green 
+            document.querySelector('#green').onclick = function()
+                  {
+                      document.querySelector('#test').style.color='green';           
+                  }
+         
+          }
+
+          {
+         
+            document.querySelector('#bg').onclick = function()
+           
+                   {
+                    document.body.style.backgroundColor = "black";
+                    }
+
+            
+           
+                             
+          }
 
       
     
